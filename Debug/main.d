16bfa6c760c90af5543d50ee0bd4bcc1637b5f03
alexad4 <alexis.adde@hotmail.fr>
2021-03-09 09:56:51 +0100

    Fichiers de départ

---
+++ Debug/main.d
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/string.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/linkage.h
+main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/cdefs.h
+
+../main.c: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/string.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/cdefs.h: 
